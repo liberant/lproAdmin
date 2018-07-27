8113c3e6905a6573f659582417808335b350aef5
Noven Purnell-Webb <noven@liberant.com.au>
2018-07-27 08:11:19 +0700

    Business forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,46 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
+import { Component, ViewChild } from '@angular/core';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Nav, Platform } from 'ionic-angular';
 
 import { HomePage } from '../pages/home/home';
+
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html',
 })
-export class MyApp {
-  rootPage:any = HomePage;
+export class LpAdminApp {
+    @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+    rootPage: any = HomePage;
+
+    pages: { title: string, component: any }[];
+
+    constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+        this.initializeApp();
+
+        this.pages = [
+            { title: 'Dashboard', component: HomePage },
+            { title: 'Businesses', component: 'BusinessesPage' },
+            { title: 'Users', component: 'UsersPage' },
+            { title: 'Products', component: 'ProductsPage' },
+            { title: 'Winelists', component: 'WinelistsPage' },
+            { title: 'Orders', component: 'OrdersPage' },
+            { title: 'Settings', component: 'SettingsPage' },
+            ];
+    }
+
+    initializeApp() {
+        this.platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
+
+    openPage(page) {
+        // Reset the content nav to have just this page
+        // we wouldn't want the back button to show in this scenario
+        this.nav.setRoot(page.component);
+    }
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,61 @@
+import { ErrorHandler, Injectable, Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Pro } from '@ionic/pro';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { NgPipesModule } from 'ngx-pipes';
 
-import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AuthProvider } from '../providers/auth/auth';
+import { FirestoreProvider } from '../providers/firestore/firestore';
+
+import { LpAdminApp } from './app.component';
+import { firebaseConfig } from './credentials';
+
+Pro.init('91275B1A', {
+    appVersion: '0.0.1',
+});
+
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+    ionicErrorHandler: IonicErrorHandler;
+
+    constructor(injector: Injector) {
+        try {
+            this.ionicErrorHandler = injector.get(IonicErrorHandler);
+        } catch (e) {
+            // Unable to get the IonicErrorHandler provider, ensure
+            // IonicErrorHandler has been added to the providers list below
+        }
+    }
+
+    handleError(err: any): void {
+        Pro.monitoring.handleNewError(err);
+        // Remove this if you want to disable Ionic's auto exception handling
+        // in development mode.
+        this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+    }
+}
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [ LpAdminApp, HomePage ],
+    imports: [ BrowserModule, IonicModule.forRoot(LpAdminApp), AngularFireModule.initializeApp(firebaseConfig), AngularFireAuthModule, AngularFirestoreModule.enablePersistence(), AngularFireStorageModule, NgPipesModule,
+
+    ],
+    bootstrap: [ IonicApp ],
+    entryComponents: [ LpAdminApp, HomePage ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: ErrorHandler, useClass: IonicErrorHandler },
+        AuthProvider,
+        FirestoreProvider, // IonicErrorHandler,
+    ],
 })
-export class AppModule {}
+export class AppModule {
+}
